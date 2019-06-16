7d4e5c70cecb64b10bcdcc19227d1f564394b1eb
Pablo <perojas3@gmail.com>
2019-06-16 14:51:36 -0400

    Ready with problems

--- find_classifier.py
+++ find_classifier.py
@@ -1,34 +1,33 @@
 from time import time
 
 from sklearn.model_selection import GridSearchCV
-from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPClassifier as MLP
 
-from layers import ONE_LAYER, TWO_LAYER, TREE_LAYER
+from numpy import load
 
 # The data is load
-Xtest, Xtrain, ytest, ytrain = loadData()  # This one have to be changed
+# Xtest, Xtrain, ytest, ytrain = # Here have to be put the data
 
 # Is this nesesary?
-ytrain = ytrain.ravel()
+# ytrain = ytrain.ravel()
 
 # This is for capturing the best value
 start_time = time()
 
-parameter_space = {
-    'hidden_layer_sizes': ONE_LAYER + TWO_LAYER + TREE_LAYER,
-    'activation': ['identity', 'logistic', 'tanh', 'relu'],
-    'solver': ['lbfgs', 'sgd', 'adam'],
+param_grid = {
+    'hidden_layer_sizes': [(50, 50, 50), (50, 100, 50), (8, 8, 8), (8, 10, 8)],
+    'activation': ['tanh', 'relu'],
+    'solver': ['sgd', 'adam'],
     'alpha': [0.0001, 0.05],
     'learning_rate': ['constant', 'adaptive'],
 }
 
-cv = [(slice(None), slice(None))]  # Hack para no hacer cv
-mlp = MLPClassifier(early_stopping=True, validation_fraction=1.0 / 4)
-clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=cv)
+clf = GridSearchCV(MLP(early_stopping=True, validation_fraction=5.0 / 6), param_grid, cv=10)
 clf.fit(Xtrain, ytrain)
 nn_acc = clf.score(Xtest, ytest)
 
 print(f"NN: El accuracy encontrado fue {nn_acc * 100.0}%")
+
 
 elapsed_time = time() - start_time
 print("Time final: {}".format(elapsed_time))
